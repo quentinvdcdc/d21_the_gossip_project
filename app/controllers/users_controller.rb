39ee3de6ede93d8143396af99e904fa10fc73cdb
quentinvdcdc <quentin.vandecandelaere@gmail.com>
2019-02-07 17:27:54 +0100

    YOLO10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,22 @@ class UsersController < ApplicationController
   	#puts "Ceci est mon premier user : #{@user}"
   	#puts params
   end
+  def new
+    @cities = City.all
+    @user = User.new
+  end
+  def create
+    @city = City.find(1)
+    @user = User.new(first_name: params[:first_name], last_name: params[:last_name], description: params[:description], age: params[:age], email: params[:email], password: params[:password], city: @city)
+    # on vérifie si l'utilisateur existe bien ET si on arrive à l'authentifier (méthode bcrypt) avec le mot de passe 
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:success] = 'Welcome Aboard'
+      redirect_to root_path
+      # redirige où tu veux, avec un flash ou pas
+    else
+      flash.now[:danger] = 'Tu veux vraiment faire partie de la familia ?'
+      render 'new'
+    end
+  end
 end
